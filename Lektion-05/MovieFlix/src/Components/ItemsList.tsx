5ded97917aa0223e7f71bfb86ae6581eedf02ae3
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-11-14 12:04:27 +0100

    Updated solution to lunch

--- Lektion-05/MovieFlix/src/Components/ItemsList.tsx
+++ Lektion-05/MovieFlix/src/Components/ItemsList.tsx
@@ -1,5 +1,6 @@
 import { IMovie } from '../Models/IMovie';
 import { IShow } from '../Models/IShow';
+import Item from './Item';
 
 type ItemListProps = {
   items: IMovie[] | IShow[];
@@ -8,20 +9,9 @@ type ItemListProps = {
 const ItemsList = ({ items }: ItemListProps) => {
   return (
     <section className='grid'>
-      {/* {movies.map((movie) => (
-        <section className='card' key={movie.id}>
-          <NavLink to={`/movies/${movie.id}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </NavLink>
-          <div className='card-body'>
-            <h5>{movie.title}</h5>
-            <small className='text-muted'>{movie.release_date}</small>
-          </div>
-        </section>
-      ))} */}
+      {items.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
     </section>
   );
 };
