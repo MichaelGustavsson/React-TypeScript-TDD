def514aaa7c37c6b413f4889ff8695faeb72d403
MichaelGustavsson <michael.gustavsson@skaftoskorpan.se>
2024-11-29 12:14:29 +0100

    Update api server

--- Lektion-09-Angular/api/server.mjs
+++ Lektion-09-Angular/api/server.mjs
@@ -1,22 +1,22 @@
-import express from "express";
-import cors from "cors";
-import fs from "node:fs/promises";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import fs from 'node:fs/promises';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.static("images"));
+app.use(express.static('images'));
 app.use(express.json());
 app.use(cors());
 
 // endpoints...
-app.get("/api/vehicles", async (req, res) => {
-  const fileContent = await fs.readFile("./data/vehicles.json");
+app.get('/api/vehicles', async (req, res) => {
+  const fileContent = await fs.readFile('./data/vehicles.json');
   const vehicles = JSON.parse(fileContent);
 
-  res.status(200).json({ vehicles: vehicles });
+  res.status(200).json({ success: true, data: vehicles });
 });
 
 const port = process.env.PORT;
